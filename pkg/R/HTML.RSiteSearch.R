fde0818e949e53d991e52d05f8e005cfed9ecdc9
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-04 03:03:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsitesearch@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/HTML.RSiteSearch.R
@@ -0,0 +1,105 @@
+HTML <- function(x, ...) {
+  UseMethod("HTML")
+}
+
+HTML.RSiteSearch <- function(x, file, title, openBrowser = TRUE, ...) {
+  .cat <- function(...)
+    cat(..., "\n", sep = "", file = con, append = TRUE)
+  ocall <- attr(x, "call")
+  string <- eval(ocall$string)
+  if(missing(file))
+    file <- sprintf("%s.html", paste(string, collapse = "_"))
+  if(missing(title))
+    title <- string
+  dir <- dirname(file)
+  dc0 <- dir.create(dir, FALSE, TRUE)
+  con <- file(file, "wt")
+  on.exit(close(con))
+  js <- system.file("js/sorttable.js", package = "RSiteSearch")
+  if(!file.exists(js)) {
+    warning("Unable to locate 'sorttable.js' file")
+  } else {
+    file.copy(js, dir)
+  }
+  cat("<html>", file = con)
+  .cat("<head>")
+  .cat("<title>", title, "</title>")
+  .cat("<script src=sorttable.js type='text/javascript'></script>")
+  .cat("<style>
+table.sortable thead {
+  font: normal 10pt Tahoma, Verdana, Arial;
+  background: #eee;
+  color: #666666;
+  font-weight: bold;
+  cursor: hand;
+}
+table.sortable th {
+  width: 75px;
+  color: #800;
+  border: 1px solid black;
+}
+table.sortable th:hover {
+  background: #eea;
+}
+table.sortable td {
+  font: normal 10pt Tahoma, Verdana, Arial;
+  text-align: center;
+  border: 1px solid blue;
+}
+.link {
+  padding: 2px;
+  width: 600px;
+}
+.link:hover {
+  background: #eeb;
+}
+a {
+  color: darkblue;
+  text-decoration: none;
+  border-bottom: 1px dashed darkblue;
+}
+a:visited {
+  color: black;
+}
+a:hover {
+  border-bottom: none;
+}
+h1 {
+  font: normal bold 20pt Tahoma, Verdana, Arial;
+  color: #00a;
+  text-decoration: underline;
+}
+h2 {
+  font: normal bold 12pt Tahoma, Verdana, Arial;
+  color: #00a;
+}
+table.sortable .empty {
+  background: white;
+  border: 1px solid white;
+  cursor: default;
+}
+</style>
+</head>")
+  .cat("<h1>RSiteSearch Results</h1>\n")
+  .cat("<h2>call: <font color='#800'>",
+       paste(deparse(ocall), collapse = ""), "</font></h2>\n")
+  .cat("<table class='sortable'>\n<thead>")
+  link <- as.character(x$Link)
+  desc <- gsub("(^[ ]+)|([ ]+$)", "", as.character(x$Description))
+  x$Link <- sprintf("<a href='%s' target='_blank'>%s</a>", link, desc)
+  x$Description <- NULL
+  .cat("<tr>\n  <th style='width:40px'>Id</th>")
+  .cat(sprintf("  <th>%s</th>\n</tr>", paste(names(x), collapse = "</th>\n  <th>")))
+  .cat("</thead>\n<tbody>")
+  paste.list <- c(list(row.names(x)), lapply(x, as.character), sep = "</td>\n  <td>")
+  tbody.list <- do.call("paste", paste.list)
+  tbody <- sprintf("<tr>\n  <td>%s</td>\n</tr>", tbody.list)
+  tbody <- sub("<td><a", "<td class=link><a", tbody)
+  .cat(tbody)
+  .cat("</tbody>
+</table>
+</body>
+</html>")
+  browseURL(file)
+  invisible()
+}
---
+++ pkg/R/summary.RSiteSearch.R
@@ -0,0 +1,24 @@
+summary.RSiteSearch <- function(object, threshold = 1, ...) {
+  Sum <- attr(object, 'PackageSummary')
+  sel <- (Sum[, 'Count'] >= threshold)
+  sumTh <- Sum[sel,, drop=FALSE]
+  structure(list(PackageSummary = sumTh,
+                 threshold = threshold,
+                 hits = attr(object, "hits"),
+                 nrow = nrow(object),
+                 call = attr(object, "call")),
+            class = c("summary.RSiteSearch", "list"))
+}
+
+print.summary.RSiteSearch <- function(x, ...) {
+  cat("\nCall:\n")
+  cat(paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
+  cat("Total number of hits: ", x$hits, "\n", sep = "")
+  cat("Number of links downloaded: ", x$nrow, "\n\n", sep = "")
+  cat("Packages with at least ", x$threshold, " hit",
+      if(x$threshold == 1) "" else "s",
+      " using search pattern '",
+      x$call$string, "':\n", sep = "")
+  print(x$PackageSummary)
+  invisible()
+}
