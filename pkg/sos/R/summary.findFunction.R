9c348cb8fd3df17e2131fc3a120d6428bfb0904d
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-30 02:16:50 +0000

    minor edit to summary.findFunction
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsitesearch@77 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sos/R/summary.findFunction.R
+++ pkg/sos/R/summary.findFunction.R
@@ -17,8 +17,8 @@ print.summary.findFunction <- function(x, ...) {
   cat(paste(deparse(x$call), sep = "\n", collapse = "\n"),
       "\n\n", sep = "")
   cat("Total number of matches: ", sum( x$matches) , "\n", sep = "")
-  cat('Downloaded', x$nrow, 'links in', x$nPackages,
-      "packages.\n\n")
+  cat('Downloaded ', x$nrow, ' links in ', x$nPackages,
+      " package", c('.', 's.')[1+(x$nPackages>1)], "\n\n", sep='')
   string <- x$string
   cat("Packages with at least ", x$threshold, " match",
       if(x$threshold == 1) "" else "es",
