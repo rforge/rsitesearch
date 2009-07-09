a71650ffbc0ec64ad5e3c7885f968642052e70b6
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-09 03:34:04 +0000

    PackageSum2 w. vignettes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsitesearch@85 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sos/R/PackageSum2.R
+++ pkg/sos/R/PackageSum2.R
@@ -1,24 +1,24 @@
 PackageSum2 <- function(x,
-      fields=c("Title", "Version", "Author", "Maintainer", "Packaged"),
-                        lib.loc=NULL, ...){
+      fields=c("Title", "Version", "Author", "Maintainer", "Packaged",
+          'vignette'), lib.loc=NULL, ...){
   UseMethod('PackageSum2')
 }
 
 PackageSum2.findFn <- function(x,
-      fields=c("Title", "Version", "Author", "Maintainer", "Packaged"),
-                        lib.loc=NULL, ...){
+      fields=c("Title", "Version", "Author", "Maintainer", "Packaged",
+          'vignette'), lib.loc=NULL, ...){
   PackageSum2(attr(x, 'PackageSummary'), fields, lib.loc, ...)
 }
 
 PackageSum2.list <- function(x,
-      fields=c("Title", "Version", "Author", "Maintainer", "Packaged"),
-                        lib.loc=NULL, ...){
+      fields=c("Title", "Version", "Author", "Maintainer", "Packaged",
+          'vignette'), lib.loc=NULL, ...){
   PackageSum2(x$PackageSummary, fields, lib.loc, ...)
 }
 
 PackageSum2.data.frame <- function(x,
-      fields=c("Title", "Version", "Author", "Maintainer", "Packaged"),
-                        lib.loc=NULL, ...){
+      fields=c("Title", "Version", "Author", "Maintainer", "Packaged",
+          'vignette'), lib.loc=NULL, ...){
 ##
 ## 1.  Create character matrix for fields
 ##
@@ -53,6 +53,19 @@ PackageSum2.data.frame <- function(x,
           if(fields[ic] %in% names(pkgDesci))
             xnew[ip, ic] <- pkgDesci[[fields[ic]]]
 #            xout[ip, fields[ic]] <- pkgDesci[[fields[ic]]]
+        else {
+          if(fields[ic] == 'vignette') {
+            vig <- (vignette(package=x$Package[ip])$results)
+            {
+              if(nrow(vig)>1){
+                clps <- paste(vig[, 'Item'], collapse=', ')
+                xnew[ip, ic] <- paste(nrow(vig), clps, sep=':  ')
+              }
+              else if(nrow(vig)>0)
+                xnew[ip, ic] <- vig[, 1, 'Title']
+            }
+          }
+        }
       }
     }
   }
