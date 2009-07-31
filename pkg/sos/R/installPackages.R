ded489f1669cce118011e5c337566a4f2e69bedf
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-31 10:08:44 +0000

    installPackages:  fix pblem
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsitesearch@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sos/R/installPackages.R
+++ pkg/sos/R/installPackages.R
@@ -1,8 +1,19 @@
 installPackages <- function(x, minCount=sqrt(x[['Count']][1]),
                                     ...){
+##
+## 1.  pkgs needed
+##
   pkgsum <- attr(x, 'PackageSummary')
   sel <- (pkgsum$Count > minCount)
-  toget <- pkgsum$Package
-  install.packages(toget)
+  toget <- pkgsum$Package[sel]
+##
+## 2.  Installed pkgs?
+##
+  instPkgs <- .packages(TRUE)
+  notInst <- toget[!(toget %in% instPkgs)]
+##
+## 3.  get not installed
+##
+  if(length(notInst)>0) install.packages(notInst)
 }
 
