9b8597bf980d6d0ddb10b5ff70052c5bc49e0f75
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-09 14:24:49 +0000

    error msgs -> installPackages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsitesearch@95 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sos/R/installPackages.R
+++ pkg/sos/R/installPackages.R
@@ -4,6 +4,14 @@ installPackages <- function(x, minCount=sqrt(x[['Count']][1]),
 ## 1.  pkgs needed
 ##
   pkgsum <- attr(x, 'PackageSummary')
+  xName <- substring(deparse(substitute(x)), 1, 25)
+  if(is.null(pkgsum))
+    stop('not a findFn object;  does not have ',
+         'attribute PackageSummary:  ', xName)
+  if(!is.data.frame(x))
+    stop('not a findFn object:  is not a data.frame:  ', xName)
+  if(!all(c('Count', 'Package') %in% names(x)))
+    stop('Must have columns Count & Package:  ', xName)
   sel <- (pkgsum$Count >= minCount)
   toget <- pkgsum$Package[sel]
 ##
