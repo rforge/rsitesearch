e4bc0f97e78910bda83c1e7f75a9b84d91aebe07
landroni <landroni@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-03 23:22:09 +0000

    add support for writeFindFn2xls()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsitesearch@164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrPlugin.sos/R/sosInit.R
+++ pkg/RcmdrPlugin.sos/R/sosInit.R
@@ -8,14 +8,18 @@ sosInit <- function(){
     searchFrame <- tkframe(top)
     searchInput <- tclVar("")
     searchField <- tkentry(searchFrame, width="30", textvariable=searchInput)
-    ### TODO diff eqs exp
-    tk2tip(searchField, paste('You can specify up to 9 search terms separated \neither by "|" (vertical bars, to obtain the union \nof results) or "&" (ampersands, to obtain the \nintersection). Use only one type of separating \noperator per search.', sep=""))
+    searchFieldTip <- paste('You can specify up to 9 search terms separated \neither by "|" (vertical bars, to obtain the union \nof results) or "&" (ampersands, to obtain the \nintersection). Use only one type of separating \noperator per search.', sep="")
+    tk2tip(searchField, searchFieldTip)
     maxpageNumber <- tclVar("20")
     maxpageNumSlider <- tkscale(searchFrame, from=1, to=99, showvalue=TRUE,
       variable=maxpageNumber, resolution=1, orient="horizontal"
       )
     summaryVariable <- tclVar("0")
     summaryCheckBox <- tkcheckbutton(searchFrame, variable=summaryVariable)
+    xlsVariable <- tclVar("0")
+    xlsCheckBox <- tkcheckbutton(searchFrame, variable=xlsVariable)
+    xlsCheckBoxTip <- paste('writeFindFn2xls() reports additional information \non installed packages (available vignettes, etc.). \nNOTE: The resulting file will be (over)written to \n".sos.xls" (or to ".sos.csv" in some cases). ', sep="")
+    tk2tip(xlsCheckBox, xlsCheckBoxTip)
     require("sos")
     sosGrep(recall=sosInit, bLabel=gettextRcmdr("Filter by:"), eLabel=gettextRcmdr("in"), 
       initialLabel=gettextRcmdr("Filter by"))
@@ -23,6 +27,7 @@ sosInit <- function(){
         search <- paste(tclvalue(searchInput))
         maxpage <- tclvalue(maxpageNumber)
         summary <- paste(tclvalue(summaryVariable))
+        xls <- paste(tclvalue(xlsVariable))
         closeDialog()
         vbar <- grepl("|", search, fixed=TRUE)
         ampersand <- grepl("&", search, fixed=TRUE)
@@ -68,9 +73,9 @@ sosInit <- function(){
         }
         logger(paste("## Launching RSiteSearch with ", length(search), 
           " search term(s). Please be patient..", sep=""))
-        if(summary == "0" & .grep=="" & length(search)==1){
+        if(summary == "0" & xls == "0" & .grep=="" & length(search)==1){
             doItAndPrint(paste(searchCommand, sep=""))
-        } else if(summary == "1" | .grep!="" | length(search)>1){
+        } else if(summary == "1" | xls == "1" | .grep!="" | length(search)>1){
             if(length(search)==1){
                 doItAndPrint(paste(".sos", " <- ", searchCommand, sep=""))
             } else {
@@ -96,6 +101,9 @@ sosInit <- function(){
                 if(summary == "1"){
                     doItAndPrint(paste("summary(.sos)", sep=""))
                 }
+                if(xls == "1"){
+                    doItAndPrint(paste("writeFindFn2xls(.sos)", sep=""))
+                }
             }
             if(length(search)==1){
                 doItAndPrint(paste("remove(.sos)", sep=""))
@@ -108,10 +116,11 @@ sosInit <- function(){
     }
     OKCancelHelp(helpSubject="findFn")
     tkgrid(tklabel(searchFrame, text=gettextRcmdr("Enter a search term..."), fg="blue"), sticky="w")
-    tkgrid(tklabel(searchFrame, text=gettextRcmdr("String:")), searchField, sticky="w")
+    tkgrid(tk2label(searchFrame, tip=searchFieldTip, text=gettextRcmdr("String:")), searchField, sticky="w")
     tkgrid(labelRcmdr(searchFrame, text=gettextRcmdr("Maximum pages:")),
       maxpageNumSlider, sticky="sw")
     tkgrid(tklabel(searchFrame, text=gettextRcmdr("Summary")), summaryCheckBox, sticky="w")
+    tkgrid(tk2label(searchFrame, tip=xlsCheckBoxTip, text=gettextRcmdr("Write to spreadsheet")), xlsCheckBox, sticky="w")
     tkgrid(tklabel(searchFrame, text=gettextRcmdr(" ")), grepButton, sticky="w")
     tkgrid(searchFrame, sticky="w")
 #    tkgrid(grepFrame, sticky="w")
